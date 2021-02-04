fc6672991cd7d5073e81040a5765634251ef570a
Stuzerain <shanford3@gmail.com>
2021-02-04 15:36:50 -0500

    cleanup of asyncForEach in mcQuery.js

--- server/mcQuery.js
+++ server/mcQuery.js
@@ -78,7 +78,7 @@ const pgUpdate = (name) => {
 
 const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
 
-async function asyncForEach(array, callback) {
+const asyncForEach = async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
