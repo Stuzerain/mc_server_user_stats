86171d7cf6ed223c3e920c15c7b88bb1e0c8e268
Stuzerain <shanford3@gmail.com>
2021-02-04 10:57:06 -0500

    server endpoint to query mc server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.use(express.json());
 
 app.use('/', express.static('./client/public'));
 
 app.get('/test', (req, res) => {
-  return res.json('test successful')
+  axios.get(`https://api.mcsrvstat.us/2/${process.env.SERVER_IP}`)
+    .then(result => res.json(result.data.players))
+
+
+    /* EXAMPLE RESPONSE:
+    {
+      "online": 1,
+      "max": 20,
+      "list": [
+          "<NAME>"
+      ],
+      "uuid": {
+          "<NAME>": "<some nonsense>"
+      }
+    }
+   */
 });
 
 module.exports = app;
+
+// https://api.mcsrvstat.us/2/73.200.234.234:25565
