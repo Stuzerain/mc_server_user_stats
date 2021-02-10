229437b525d7d6256d48fb87e4b8051ac8e8807f
Stuzerain <shanford3@gmail.com>
2021-02-10 13:47:59 -0500

    separated main App.jsx into Header and Body components

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import ChartView from './ChartView.jsx';
+import TableView from './TableView.jsx';
+import DateSearch from './DateSearch.jsx';
+
+
+
+const Body = ( { display } ) => {
+  const [data, setData] = useState([]);
+  const [currentlyOnline, setCurrentlyOnline] = useState([]);
+
+  const checkTotals = () => {
+    axios.get('/api/totals')
+      .then(results => {
+        setData(results.data);
+      })
+  }
+
+  const checkOnline = () => {
+    axios.get('/api/online')
+    .then(results => {
+      if (results.data.online > 0) {
+        setCurrentlyOnline(results.data.list)
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkOnline();
+    checkTotals();
+  }, [])
+
+  return (
+    <div>
+      <TableView data={data} currentlyOnline={currentlyOnline} checkOnline={checkOnline}/>
+      <ChartView data={data} checkTotals={checkTotals}/>
+      <DateSearch data={data}/>
+    </div>
+  )
+}
+
+export default Body;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Header = ( { setDisplay } ) => {
+
+  return (
+    <header>
+      Some text
+    </header>
+  )
+}
+
+export default Header;
